b9d020015f6bfc7ea8b59cf4e9f9375828d74b53
SvetlanaReutova <reutova_sveta@ukr.net>
2020-09-30 19:59:01 +0200

    Updates

---
+++ precache-manifest.f120f434527662870909553f8e4ce68a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318188b9e7307c320b91895792dcc672",
+    "url": "/ITN_Bootcamp/index.html"
+  },
+  {
+    "revision": "f8c9f7927f73d3b3d2bd",
+    "url": "/ITN_Bootcamp/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "7d5a0d85c92194d58f09",
+    "url": "/ITN_Bootcamp/static/css/main.ea94b00a.chunk.css"
+  },
+  {
+    "revision": "f8c9f7927f73d3b3d2bd",
+    "url": "/ITN_Bootcamp/static/js/2.a1a9dc96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ITN_Bootcamp/static/js/2.a1a9dc96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5a0d85c92194d58f09",
+    "url": "/ITN_Bootcamp/static/js/main.463847a7.chunk.js"
+  },
+  {
+    "revision": "80c04dfed03e3f653ac0",
+    "url": "/ITN_Bootcamp/static/js/runtime-main.86359aa5.js"
+  },
+  {
+    "revision": "a3fe3aa17418ceb9f8e4c1961faf77e0",
+    "url": "/ITN_Bootcamp/static/media/city_planning.a3fe3aa1.jpg"
+  },
+  {
+    "revision": "1d9ae9e91593976f92150a639bbdf321",
+    "url": "/ITN_Bootcamp/static/media/energetics.1d9ae9e9.jpg"
+  },
+  {
+    "revision": "17b056bde9ed2137fe8aec437de3fec4",
+    "url": "/ITN_Bootcamp/static/media/energy.17b056bd.jpg"
+  },
+  {
+    "revision": "ca45f18c6a4b07e95cb22f43e9250611",
+    "url": "/ITN_Bootcamp/static/media/hero-bg-iPhone.ca45f18c.jpg"
+  },
+  {
+    "revision": "b236af76d3e12b9f2dc0a2d673375d86",
+    "url": "/ITN_Bootcamp/static/media/hero-bg.b236af76.jpg"
+  },
+  {
+    "revision": "3d66b58c57513e79022af1cbb2936e5f",
+    "url": "/ITN_Bootcamp/static/media/industry.3d66b58c.jpg"
+  },
+  {
+    "revision": "3e6d941cbfa160cc45b27d1288d9d561",
+    "url": "/ITN_Bootcamp/static/media/slide-1.3e6d941c.jpg"
+  },
+  {
+    "revision": "2d01e3239b6bf044acb33d0c494d140c",
+    "url": "/ITN_Bootcamp/static/media/slide-2.2d01e323.jpg"
+  },
+  {
+    "revision": "2e8165b3ecf02d46bd2229aeeef76b9f",
+    "url": "/ITN_Bootcamp/static/media/slide-3.2e8165b3.jpg"
+  },
+  {
+    "revision": "4bfc613740f98e03bb39a65ba436d672",
+    "url": "/ITN_Bootcamp/static/media/slide-4.4bfc6137.jpg"
+  },
+  {
+    "revision": "b2936b7dcfaeb3423ea4a3123ef85f39",
+    "url": "/ITN_Bootcamp/static/media/slide-5.b2936b7d.jpg"
+  },
+  {
+    "revision": "fb06d4dd85a3187e7d299e96bd5393b8",
+    "url": "/ITN_Bootcamp/static/media/transport.fb06d4dd.jpg"
+  },
+  {
+    "revision": "19cb675508b5ffec4024e651a6e5ec0e",
+    "url": "/ITN_Bootcamp/static/media/utilization.19cb6755.jpg"
+  }
+]);
